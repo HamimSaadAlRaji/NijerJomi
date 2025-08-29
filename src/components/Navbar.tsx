1e977d3758259bda036268b868402977f4120ddc
KHALID9029 <khalidhasan@iut-dhaka.edu>
2025-08-30 00:58:41 +0600

    LOGO

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,7 +73,7 @@ const Navbar = () => {
   return (
     <nav className={getNavbarStyles()}>
       <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <NavbarLogo isDarkTheme={isDarkTheme} />
 
